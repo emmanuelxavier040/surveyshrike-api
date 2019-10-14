71091debaf760b602d04ea2ca15f13bd2b3219e8
Emmanuel Xavier <emmanuelxavier040@gmail.com>
2019-10-14 12:20:46 +0530

    Fix on appspec.yml

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -3,4 +3,4 @@
 set -e
 
 
-nohup java -jar /tmp/surveyshrike-api-0.0.1-SNAPSHOT.jar "-Dspring.config.location=file:./application.properties" &
+nohup java -jar /tmp/surveyshrike-api-0.0.1-SNAPSHOT.jar "-Dspring.config.location=file:./application.properties" >/dev/null 2>&1 &
